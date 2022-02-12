a4563d7cd11abbd1cf105c1200ac163757614016
Ryo <try0.development@gmail.com>
2022-02-12 21:37:02 +0900

    オプション拡張
    ・ヘッダー、フッター
    ・吹き出しスタイル
    ・プログレスステータス自動付与
    
    他微修正

--- src/simple-timeline.js
+++ src/simple-timeline.js
@@ -7,6 +7,28 @@ import "./simple-timeline.scss"
  */
 export default class SimpleTimeline {
 
+
+    static defaultOption = {
+        debug: false,
+        // Renders a line reflecting the state of the timeline item.
+        progress: false,
+        // Invertes the progress of the line.
+        progressInverted: false,
+        // Set the state of a timeline item by comparing its date with the current date.
+        autoProgress: false,
+        dateFormatter: (date) => date.toLocaleDateString(),
+        // Label to display when the item's date is the same as the current date.
+        currentLabel: null,
+        header: null,
+        footer: null
+    };
+
+    static defaultItemOption = {
+        backgroundColor: null,
+        borderColor: null,
+        fontColor: null,
+    }
+
     /**
      * Container element
      */
@@ -18,17 +40,14 @@ export default class SimpleTimeline {
     timeline;
 
     /**
-     * Timeline setting object
+     * Timeline option object
      */
-    setting = {
-        progress: true,
-        progressInverted: false,
-    };
+    option = {};
 
     /**
      * Timeline items array
      */
-    timelineItems = [];
+    items = [];
 
     /**
      * Initializes simple-timeline
@@ -37,25 +56,43 @@ export default class SimpleTimeline {
      * @param {*} args 
      */
     constructor(container, args) {
+
         this.container = container;
 
         if (args != null) {
-            if (args.setting != null) {
-                this.setting = args.setting;
+            if (args.option != null) {
+                this.option = args.option;
             }
-            if (args.timelineItems != null) {
-                this.timelineItems = args.timelineItems;
+            if (args.items != null) {
+                this.items = args.items;
             }
         }
 
+        this.option = this.#getMergeOption(this.option);
+
+        this.initializeStyleSheet();
         this.renderTimeline();
     }
 
+    initializeStyleSheet() {
+        //ドキュメントスタイルの取得
+        const sheets = document.styleSheets;
+        const sheet = sheets[sheets.length - 1];
+
+        if (this.option.currentLabel != null) {
+            //スタイルルールの追加
+            sheet.insertRule(
+                ".st-timeline .st-item-container.st-current .st-datetime::before { content: '" + this.option.currentLabel + "' !important }",
+                sheet.cssRules.length
+            );
+        }
+
+    }
 
     /**
      * Clears timeline item elements
      */
-    clearTimelineItems() {
+    clearitems() {
 
         if (this.container.firstChild) {
             this.container.removeChild(this.container.firstChild);
@@ -64,11 +101,11 @@ export default class SimpleTimeline {
         this.timeline = document.createElement("div");
         this.timeline.className = "st-timeline";
 
-        if (this.setting.progress != null && !this.setting.progress) {
+        if (this.option.progress != null && !this.option.progress) {
             this.timeline.classList.add("st-no-progress");
         }
 
-        if (this.setting.progressInverted) {
+        if (this.option.progressInverted) {
             this.timeline.classList.add("st-inverted-progress");
         }
 
@@ -80,13 +117,42 @@ export default class SimpleTimeline {
      */
     renderTimeline() {
 
-        this.clearTimelineItems();
+        this.clearitems();
 
-        var timelineItems = this.getTimelineItems();
-        for (var i = 0; i < timelineItems.length; i++) {
-            const timelineItem = timelineItems[i];
+        // render header
+        if (this.option.header) {
+            const headerContainer = document.createElement("div");
+            headerContainer.className = "st-header-container";
 
-            this.appendItem(timelineItem);
+            const header = document.createElement("span");
+            header.className = "st-header";
+            header.innerHTML = this.option.header.label;
+
+            headerContainer.appendChild(header);
+
+            this.timeline.appendChild(headerContainer);
+        }
+
+        // render timeline items
+        const currentDate = this.option.autoProgress ? new Date() : null;
+        const items = this.getItems();
+        for (var i = 0; i < items.length; i++) {
+            const timelineItem = items[i];
+            this.appendItem(timelineItem, currentDate);
+        }
+
+        // render footer
+        if (this.option.footer) {
+            const footerContainer = document.createElement("div");
+            footerContainer.className = "st-footer-container";
+
+            const footer = document.createElement("span");
+            footer.className = "st-footer";
+            footer.innerHTML = this.option.footer.label;
+
+            footerContainer.appendChild(footer);
+
+            this.timeline.appendChild(footerContainer);
         }
     }
 
@@ -96,43 +162,74 @@ export default class SimpleTimeline {
      * @param {*} timelineItem 
      * @returns 
      */
-    appendItem(timelineItem) {
-        var itemContainerElm = document.createElement("div");
+    appendItem(timelineItem, currentDate = null) {
+
+        const itemDate = this.#getItemDate(timelineItem);
+
+        const itemContainerElm = document.createElement("div");
         itemContainerElm.className = "st-item-container";
 
-        if (timelineItem.passed) {
-            itemContainerElm.classList.add("st-passed");
+        itemContainerElm.setAttribute("data-st-date", itemDate.toISOString());
+
+        if (this.option.autoProgress && currentDate != null) {
+            if (itemDate.getTime() < currentDate.getTime()) {
+                itemContainerElm.classList.add("st-passed");
+            }
+            if (this.isSameDay(itemDate, currentDate)) {
+                itemContainerElm.classList.add("st-current");
+            }
+
+        } else {
+
+            if ("passed" == timelineItem.state) {
+                itemContainerElm.classList.add("st-passed");
+            }
+
+            if ("current" == timelineItem.state) {
+                itemContainerElm.classList.add("st-current");
+            }
         }
 
-        if (timelineItem.current) {
-            itemContainerElm.classList.add("st-current");
-        }
 
-        var lineElm = document.createElement("div");
+        const lineElm = document.createElement("div");
         lineElm.className = "st-line";
         itemContainerElm.appendChild(lineElm);
 
-        var linePointElm = document.createElement("div");
+        const linePointElm = document.createElement("div");
         linePointElm.className = "st-point ";
         itemContainerElm.appendChild(linePointElm);
 
-        var itemWraperElm = document.createElement("div");
-        itemWraperElm.className = "st-item-wraper ";
+        const itemWraperElm = document.createElement("div");
+        itemWraperElm.className = "st-item-wraper";
 
-        var itemElm = document.createElement("div");
-        itemElm.className = "st-item ";
+        const itemElm = document.createElement("div");
+        itemElm.className = "st-item";
         itemWraperElm.appendChild(itemElm);
 
-        var datetimeElm = document.createElement("p");
-        datetimeElm.innerHTML = timelineItem.datetime;
+        const itemOption = this.#getItemOption(timelineItem);
+        if (itemOption.backgroundColor != null) {
+            itemElm.style.background = itemOption.backgroundColor;
+            itemElm.style.borderColor = itemOption.backgroundColor;
+        }
+        if (itemOption.borderColor != null) {
+            itemElm.style.border = "solid 2px " + itemOption.borderColor;
+        }
+        if (itemOption.fontColor != null) {
+            itemElm.style.color = itemOption.fontColor;
+        }
+
+        const datetimeElm = document.createElement("p");
+
+        const dateFormatter = this.option.dateFormatter ?? this.defaultDateFormatter;
+        datetimeElm.innerHTML = dateFormatter(itemDate);
         datetimeElm.className = "st-datetime";
 
 
-        var titleElm = document.createElement("p");
+        const titleElm = document.createElement("p");
         titleElm.innerHTML = timelineItem.title;
         titleElm.className = "st-title";
 
-        var contentElm = document.createElement("div");
+        const contentElm = document.createElement("div");
         contentElm.innerHTML = timelineItem.content;
         contentElm.className = "st-content"
 
@@ -146,38 +243,81 @@ export default class SimpleTimeline {
         return itemContainerElm;
     }
 
+
     /**
      * Gets the timeline item array
      * @returns 
      */
-    getTimelineItems() {
-        return this.timelineItems;
+    getItems() {
+        return this.items;
     }
 
     /**
      * Sets timeline items
      * 
-     * @param {*} timelineItems 
+     * @param {*} items 
      */
-    setTimelineItems(timelineItems) {
-        this.timelineItems = timelineItems;
+    setItems(items) {
+        this.items = items;
     }
 
     /**
-     * Gets the timeline setting object
+     * Gets the timeline option object
      * @returns 
      */
-    getSetting() {
-        return this.setting;
+    getOption() {
+        return this.option;
     }
 
     /**
-     * Sets setting object
+     * Sets option object
      * 
-     * @param {*} setting 
+     * @param {*} option 
      */
-    setSetting(setting) {
-        this.setting = setting;
+    setOption(option) {
+        this.option = this.#getMergeOption(option);
     }
 
+    isSameDay(d1, d2) {
+        return d1.getFullYear() === d2.getFullYear() &&
+            d1.getMonth() === d2.getMonth() &&
+            d1.getDate() === d2.getDate();
+    }
+
+
+
+    #getItemOption(timelineItem) {
+        let merged = {}
+        merged = Object.assign(merged, SimpleTimeline.defaultItemOption);
+
+        if (this.#hasProperty(timelineItem, "option")) {
+            merged = Object.assign(merged, timelineItem.option);
+        }
+
+        return merged;
+    }
+
+    #hasProperty(obj, propName) {
+        if (obj == null) {
+            return false;
+        }
+
+        return Object.prototype.hasOwnProperty.call(obj, propName)
+    }
+
+    #getItemDate(timelineItem) {
+        let itemDate = timelineItem.date;
+        if (!(itemDate instanceof Date)) {
+            itemDate = new Date(itemDate);
+        }
+        return itemDate;
+    }
+
+
+    #getMergeOption(priorityOpition) {
+        let merged = {};
+        merged = Object.assign(merged, SimpleTimeline.defaultOption);
+        merged = Object.assign(merged, priorityOpition);
+        return merged;
+    }
 }
